a220b1c0465f47cb760eae2ae360702833a9344e
jungkookds <sinambelakelli2612@gmail.com>
2024-01-26 11:10:41 +0700

    CREATE file

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+  //variabel inisialisasi
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $database = "perpus";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $database);
+
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully";
+?>
